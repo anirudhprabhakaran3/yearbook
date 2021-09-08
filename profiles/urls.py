0f105a088defd17197ea12ce6d49d769697ff7e8
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2021-09-08 13:05:54 +0530

    Added login and logout

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import login
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('profiles/', views.profile_home, name='profiles_home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,3 +13,49 @@ def index(request):
         'users_count': users_count,
     }
     return render(request, 'profiles/index.html', args)
+
+def signup(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('profiles_home')
+    args = {
+        'form': form,
+    }
+    return render(request, 'profiles/signup.html', args)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "You are logged in!")
+                return redirect('profiles_home')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    args = {
+        'form': form
+    }
+    return render(request, 'profiles/login.html', args)
+
+
+@login_required
+def profile_home(request):
+    return render(request, 'profiles/profile_home.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect("index")
